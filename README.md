43263a47d10d6f2ab3bad0efddca86a7b17740a6
Zernab Gohar <zernabgohar@gmail.com>
2024-09-27 16:48:00 -0400

    Boilerplate project directory setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # user-manager
 A small full-stack application that allows users to submit a form with their name and email, which is stored in a database and displayed in a table on the front-end.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ server/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
